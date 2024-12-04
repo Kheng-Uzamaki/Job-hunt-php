d68a60bb1d2b17d75267a707f0e5b786bb509fa1
Kheng-Uzamaki <khenguzamiki1@gmail.com>
2024-12-04 16:45:54 +0700

    namespaces

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 require __DIR__ . '/../vendor/autoload.php';
 require '../helpers.php';
 
+use Framework\Router;
 // Use Autoloader instead of require() to 
 // require basePath('Framework/Router.php');
 // require basePath('Framework/Database.php');
