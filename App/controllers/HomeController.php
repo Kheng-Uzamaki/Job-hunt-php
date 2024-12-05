f0eaa2238e843b7fceed0b77fe12705788d03b21
Kheng-Uzamaki <khenguzamiki1@gmail.com>
2024-12-05 11:08:58 +0700

    refactor home and listing to controller

--- App/controllers/HomeController.php
+++ App/controllers/HomeController.php
@@ -2,12 +2,26 @@
 
 namespace App\Controllers;
 
+use Framework\Database;
+
 class HomeController
 {
-    public function __construct() {}
+    protected $db;
+
+    public function __construct()
+    {
+        $config = require basePath('config/db.php');
+        $this->db = new Database($config);
+    }
 
     public function index()
     {
-        echo "Welcome to the HomeController!";
+
+        $listings = $this->db->query('SELECT * FROM listings LIMIT 6')->fetchAll();
+
+
+        loadView('home', [
+            'listings' => $listings
+        ]);
     }
 }
